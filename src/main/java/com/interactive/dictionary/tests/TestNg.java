a6ddd3a35e0b607159c405c4ce13b9e7151e2bf2
Jakub Zamojski <kuba.zamojski@gmail.com>
2018-11-29 17:47:07 +0100

    Initial commit

---
+++ src/main/java/com/interactive/dictionary/tests/TestNg.java
@@ -0,0 +1,12 @@
+package com.interactive.dictionary.tests;
+
+import org.testng.annotations.Test;
+
+public class TestNg {
+
+	@Test
+	public void Test1() {
+		System.out.println("Hello world!!");
+	}
+	
+}
